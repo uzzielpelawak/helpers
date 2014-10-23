9e2c5295dfa0ff2ccace9b250a3c44927296ddc0
webvimark <webvimark@gmail.com>
2014-10-23 15:10:07 +0200

    getRealIp

--- LittleBigHelper.php
+++ LittleBigHelper.php
@@ -103,4 +103,40 @@ class LittleBigHelper
 
 		return $lowercase ? strtolower($string) : $string;
 	}
+
+	/**
+	 * Find real user IP
+	 *
+	 * @return string|null
+	 */
+	public static function getRealIp()
+	{
+		$ip = null;
+
+		if (php_sapi_name() == 'cli')
+		{
+			return $ip;
+		}
+		else
+		{
+			$client  = @$_SERVER['HTTP_CLIENT_IP'];
+			$forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+			$remote  = @$_SERVER['REMOTE_ADDR'];
+
+			if(filter_var($client, FILTER_VALIDATE_IP))
+			{
+				$ip = $client;
+			}
+			elseif(filter_var($forward, FILTER_VALIDATE_IP))
+			{
+				$ip = $forward;
+			}
+			else
+			{
+				$ip = $remote;
+			}
+
+			return $ip;
+		}
+	}
 } 
